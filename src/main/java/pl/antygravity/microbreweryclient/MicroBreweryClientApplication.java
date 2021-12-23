04fe967cf743c23ff34adde414bb468de26c56b7
Andrzej Rawski <rawski.andrzej@gmail.com>
2021-12-23 23:18:31 +0100

    Initial Commit

---
+++ src/main/java/pl/antygravity/microbreweryclient/MicroBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package pl.antygravity.microbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroBreweryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/antygravity/microbreweryclient/MicroBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.antygravity.microbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
